c5e0c99bec33672cd9042778f0f32e6b87e46fe9
Multikabel <michal.racicky@seznam.cz>
2026-01-05 23:52:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -81,16 +81,6 @@ elif volba == "Analýza týmu":
 
 elif volba == "Nadcházející zápasy":
     st.header("Kalendář zápasů")
-try: df_fixtures = pd.read_csv('kalendar_budouci2.csv')
-    sep =';'
-
-    # Pokud má CSV specifický oddělovač (např. středník), přidej: sep=';'
-    
-    # Zobrazení ve formě interaktivní tabulky
-    st.dataframe(df_fixtures, use_container_width=True, hide_index=True)
-
-except FileNotFoundError:
-    st.error("Soubor s nadcházejícími zápasy nebyl nalezen.")
     if df_kal is not None and not df_kal.empty:
         st.table(df_kal)
     else:
