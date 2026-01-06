db678224cdf276a67f8816997350e918662ec5d9
Multikabel <michal.racicky@seznam.cz>
2026-01-06 14:43:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,35 +8,31 @@ PATH_TABULKA = 'PL_tabulka_aktualni.csv'
 PATH_KALENDAR = 'PL_kalendar_budouci2.csv'
 
 
-
 LOGA_TYMU = {
-    "Arsenal": "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg",
-    "Aston Villa": "https://play-lh.googleusercontent.com/6J_v7Vn-G477XG1N_vR3S6UvjVnL8pC9pPqI_W6mN6z3P5L5L5L5L5L5L5L5L5L5L5",
+    "Arsenal": "https://play-lh.googleusercontent.com/9m0-z_Uo373Xn09P1T40XbE_W4K_36nOaL66H4WvP_Hl3_Dq1hG_M_qXvV-WbO9e9A=w240-h480-rw",
+    "Aston Villa": "https://img.vavel.com/t-aston-villa-logo-png-4336-1603912197545.png",
     "Bournemouth": "https://upload.wikimedia.org/wikipedia/en/e/e5/AFC_Bournemouth_%282013%29.svg",
     "Brentford": "https://upload.wikimedia.org/wikipedia/en/2/2a/Brentford_FC_crest.svg",
     "Brighton": "https://upload.wikimedia.org/wikipedia/en/f/fd/Brighton_%26_Hove_Albion_logo.svg",
-    "Burnley": "https://upload.wikimedia.org/wikipedia/en/6/62/Burnley_F.C._Logo.svg",
+    "Burnley": "https://crests.football-data.org/70.png",
     "Chelsea": "https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg",
     "Crystal Palace": "https://upload.wikimedia.org/wikipedia/en/a/a2/Crystal_Palace_FC_logo_%282022%29.svg",
     "Everton": "https://upload.wikimedia.org/wikipedia/en/7/7c/Everton_FC_logo.svg",
-    "Leeds United": "https://upload.wikimedia.org/wikipedia/en/5/54/Leeds_United_F.C._logo.svg",
-    "Leeds": "https://upload.wikimedia.org/wikipedia/en/5/54/Leeds_United_F.C._logo.svg",
+    "Fulham": "https://crests.football-data.org/63.png",
+    "Leeds United": "https://crests.football-data.org/341.png",
     "Liverpool": "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg",
     "Man City": "https://upload.wikimedia.org/wikipedia/en/e/eb/Manchester_City_FC_badge.svg",
-    "Man United": "https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg",
     "Man Utd": "https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg",
     "Newcastle": "https://upload.wikimedia.org/wikipedia/en/5/56/Newcastle_United_Logo.svg",
     "Nott'm Forest": "https://upload.wikimedia.org/wikipedia/en/e/e5/Nottingham_Forest_F.C._logo.svg",
     "Southampton": "https://upload.wikimedia.org/wikipedia/en/c/c9/Southampton_FC.svg",
-    "Spurs": "https://upload.wikimedia.org/wikipedia/en/b/b4/Tottenham_Hotspur.svg", # Oprava pro Tottenham
-    "Tottenham": "https://upload.wikimedia.org/wikipedia/en/b/b4/Tottenham_Hotspur.svg",
+    "Spurs": "https://crests.football-data.org/73.png",
+    "Sunderland": "https://crests.football-data.org/71.png",
+    "Tottenham": "https://crests.football-data.org/73.png",
     "West Ham": "https://upload.wikimedia.org/wikipedia/en/c/c2/West_Ham_United_FC_logo.svg",
-    "Wolves": "https://upload.wikimedia.org/wikipedia/en/f/fc/Wolverhampton_Wanderers.svg",
-    "Aston Villa": "https://upload.wikimedia.org/wikipedia/en/f/f9/Aston_Villa_FC_crest_%282016%29.svg",
-    "Sunderland": "https://upload.wikimedia.org/wikipedia/en/thumb/6/60/Sunderland_AFC_logo.svg/200px-Sunderland_AFC_logo.svg.png",
-    "Fulham": "https://upload.wikimedia.org/wikipedia/en/thumb/3/3f/Fulham_FC_%28shield%29.svg/200px-Fulham_FC_%28shield%29.svg.png",
-    "Burnley": "https://upload.wikimedia.org/wikipedia/en/thumb/6/62/Burnley_F.C._Logo.svg/200px-Burnley_F.C._Logo.svg.png"
+    "Wolves": "https://upload.wikimedia.org/wikipedia/en/f/fc/Wolverhampton_Wanderers.svg"
 }
+
 
 
 st.set_page_config(page_title="PL Analytika 2026", layout="wide")
